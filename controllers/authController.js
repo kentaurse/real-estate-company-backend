cbfd262facfc2787885ebf9921ba832c640b4fd2
kentaurse <kentaurse0212@gmail.com>
2024-10-01 13:18:58 +0800

    feat: message model and router

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,10 +3,9 @@ const User = require('../models/User');
 
 exports.login = async (req, res) => {
   try {
-    const name = req.user.name;
-    const userId = req.user.userId;
     const token = jwt.sign({ id: req.user._id }, process.env.SECURITY_KEY, { expiresIn: '24h' });
-    res.json({ token, name, userId });
+    const userInfo = req.user;
+    res.json({token, userInfo});
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
@@ -24,5 +23,14 @@ exports.register = async (req, res) => {
     res.status(200).json({message: 'Successfull.'});
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({ userId: req.params.userId });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
--- models/Message.js
+++ models/Message.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  type: String,
+  method: String,
   read: { type: Boolean, default: false },
   sender: { type: String, required: true },
+  receiver: { type: String, required: true },
   answer: { type: Boolean, default: false },
+  deliverDate: { type: Date, default: Date.now },
   file: String
 });
 
